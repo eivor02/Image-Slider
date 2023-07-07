b2c5341ad739d7afc1316b9aa34455aece9326c8
Saif <136341062+eivor02@users.noreply.github.com>
2023-07-07 18:50:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function nextSlide() {
+  showSlide(slideIndex += 1);
+}
+
+function prevSlide() {
+  showSlide(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+  showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.opacity = 0;
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.opacity = 1;
+  dots[slideIndex-1].className += " active";
+}
